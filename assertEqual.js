6fb171d49697f69b01dbc4b8b9f1b918377008e7
SaraKate51115 <sarakate51115@gmail.com>
2020-09-14 23:32:53 +0000

    Assert Equla function with emojis

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (expected === actual) {
+    console.log('🌈🌈🌈 Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('😱😱😱 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(9, 20);
+assertEqual("Bootcamp", "Bootcamp");
