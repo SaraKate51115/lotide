1a576a4ffb7700548cbd4aee9ad8e4d94f60b193
SaraKate51115 <sarakate51115@gmail.com>
2020-09-16 23:19:15 +0000

    checked arrays for equality

---
+++ eqArrays.js
@@ -0,0 +1,37 @@
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (expected === actual) {
+    console.log(`🌈🌈🌈 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😱😱😱 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//create a function called eqArrays
+//have it take in two arrays
+//have it compare the two arrays
+//if the match return true
+//if they don't match return false
+
+const eqArrays = function(oneArray, twoArray) {
+      //return oneArray.toString() === twoArray.toString();
+      //return oneArray.join() === twoArray.join();
+  for (let i = 0; i < oneArray.length; i++) {
+    if (oneArray.length !== twoArray.length || oneArray[i] !== twoArray[i]) {
+      return false;
+    }  
+  }
+  return true;
+}
+
+
+//MORE TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 4, 3], [1, 2, 3]), true); // => should PASS
