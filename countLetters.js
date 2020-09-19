91372d3b2d6698e9dd9cb6beebebf2fb3b51e048
SaraKate51115 <sarakate51115@gmail.com>
2020-09-19 01:43:27 +0000

    created function to count frequency of letters

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (expected === actual) {
+    console.log(`🌈🌈🌈 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😱😱😱 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const counts = {}; //object for letter => count mapping
+  
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence.charAt(i);
+    count = counts[letter];
+    counts[letter] = count ? count + 1 : 1; //figure out what this is doing???
+ }
+    
+    for (letter in counts) {
+      console.log(letter + counts[letter]); 
+    }
+    
+  };
+
+countLetters("lighthouse in the house");
+
+//HOW DO I STOP COUNTING SPACES???
