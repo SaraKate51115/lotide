0cf70f4d4abab6a81a5f15c51198fcb8887dcb57
SaraKate51115 <sarakate51115@gmail.com>
2020-10-28 19:27:19 +0000

    fixed errors in middle function

--- middle.js
+++ middle.js
@@ -1,3 +1,4 @@
+//TEST/ASSERTION FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const eqArrays = function(oneArray, twoArray) {
   for (let i = 0; i < oneArray.length; i++) {
     if (oneArray.length !== twoArray.length || oneArray[i] !== twoArray[i]) {
@@ -16,29 +17,24 @@ const assertArraysEqual = function(eqArrays) {
   console.log(`🌈🌈🌈 Assertion Passed`);
     }
 }
-
 //assertArraysEqual();
-//NEED TO FIGURE OUT RETURN VALUE ~~~~~ SCOPE???
+
+//ACTUAL FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const middle = function(arr) {
-  //for (index of arr) {
+  
     let middleValue = [];
     if(arr.length === 1 || arr.length <= 2){
-      console.log([]);
-    }
-    if(arr.length % 2 === 0) {
-      middleValue[0] = arr[arr.length/2 -1]
-      middleValue[1] = arr[arr.length/2]
-      console.log(middleValue[0], middleValue[1])
-        //if(middleValue[0] <= 0){
-          //console.log([]);
+      return []
+    } else if (arr.length % 2 === 0) {
+      middleValue.push(arr[arr.length/2 -1])
+      middleValue.push(arr[arr.length/2])
+      return middleValue
     } else {
-          middleValue[0] = arr[arr.length/2 - 0.5]
-          console.log(middleValue[0])
+        middleValue.push(arr[arr.length/2 - 0.5])
+        return middleValue
         }
-      
-  return middleValue
 } 
-middle([6]) // => []
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+console.log(middle([6])) // => []
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
