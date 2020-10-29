b6fe28a73c6a6edaeaecd52e1831c5ef673a65d6
SaraKate51115 <sarakate51115@gmail.com>
2020-10-29 20:01:14 +0000

    added test code to file

--- takeUntil.js
+++ takeUntil.js
@@ -1,8 +1,27 @@
+//Test/Assertion Functions
+const eqArrays = function(oneArray, twoArray) {
+  for (let i = 0; i < oneArray.length; i++) {
+    if (oneArray.length !== twoArray.length || oneArray[i] !== twoArray[i]) {
+      return false;
+    }  
+  }
+  return true;
+}
+
+const assertArraysEqual = function(eqArrays) {
+ if (eqArrays === false) {
+  //console.log(`🌈🌈🌈 Assertion Passed`);
+  console.log(`😱😱😱 Assertion Failed`);
+  } else {
+  //console.log(`😱😱😱 Assertion Failed`);
+  console.log(`🌈🌈🌈 Assertion Passed`);
+    }
+}
+//Function Implementation~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //create takeUntil Fx that takes in 2 parameters: an array, a callback Fx
 //takeUntil will return a slice of the array with elements taken from the beginning
   //it should keep going until the callback returns a truthy value
     //the callback should only be provided one value: the item in the array
-
 const takeUntil = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -24,3 +43,8 @@ console.log(results1);
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+//Test code
+console.log(assertArraysEqual(eqArrays(results1, [1, 2, 3])))
+console.log(assertArraysEqual(eqArrays(results1, [1, 2, 5, 7, 2])))
+console.log(assertArraysEqual(eqArrays(results2, ["I've", "been", "to", "Hollywood"])))
