3ac4cffea6e4b84c9e300ae35cb2be249fdeaed5
SaraKate51115 <sarakate51115@gmail.com>
2020-10-29 19:44:37 +0000

    made code corrections and added testing

--- map.js
+++ map.js
@@ -1,3 +1,23 @@
+//Test?Assertion Functions
+const eqArrays = function(oneArray, twoArray) {
+  for (let i = 0; i < oneArray.length; i++) {
+    if (oneArray.length !== twoArray.length || oneArray[i] !== twoArray[i]) {
+      return false;
+    }  
+  }
+  return true;
+}
+
+const assertArraysEqual = function(eqArrays) {
+ if (eqArrays === false) {
+  //console.log(`🌈🌈🌈 Assertion Passed`);
+  console.log(`😱😱😱 Assertion Failed`);
+  } else {
+  //console.log(`😱😱😱 Assertion Failed`);
+  console.log(`🌈🌈🌈 Assertion Passed`);
+    }
+}
+
 
 //create a map function that takes in two arguments: an array to map, a callback function
 const map = function(arrayToMap, callback) { //return a new array based on the results of the callback function
@@ -12,4 +32,5 @@ const words = ["ground", "control", "to", "major", "tom"];
 const results1 = map(words, word => word[0]);
 console.log(results1);
 
-//assertArraysEqual tests???
+//Test
+console.log(assertArraysEqual(eqArrays(results1, ['g', 'c', 't', 'm', 't'])))
